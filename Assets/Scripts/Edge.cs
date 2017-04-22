f99c5df478811a1d731b9761eca6436dda9bb2f2
Anton Lejon <little.burrito@gmail.com>
2017-04-22 19:21:25 +0200

    Example level + edges

---
+++ Assets/Scripts/Edge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour {
+
+    public bool isClimbable = false;
+    public bool isRunnable = false;
+    public bool isCorner = false;
+
+}
