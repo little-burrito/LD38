1b5c717d58db67a553bc097fe3ce5614a62b4b5d
Anton Lejon <little.burrito@gmail.com>
2017-04-25 11:17:07 +0200

    I'm an idiot. Now the cursor hiding works.

--- Assets/CursorControl.cs
+++ Assets/CursorControl.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cursor : MonoBehaviour {
+public class CursorControl : MonoBehaviour {
 
     private float mouseSensitivity = 0.3f;
+
+    private void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void Update() {
         transform.position += new Vector3( Input.GetAxis( "Mouse X" ) * mouseSensitivity, Input.GetAxis( "Mouse Y" ) * mouseSensitivity, 0.0f );
